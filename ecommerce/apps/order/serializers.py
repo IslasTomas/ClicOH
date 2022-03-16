d71c678d01a08c4ebe3830a5809fe271e1fa75a9
capitan <tomas_islas@hotmail.com>
2022-03-16 05:09:12 -0300

    ADD: order viewset without validate

--- ecommerce/apps/order/serializers.py
+++ ecommerce/apps/order/serializers.py
@@ -1,33 +1,79 @@
+from django.db.models import Q
+
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+from rest_framework.validators import UniqueTogetherValidator
+
+from apps.product.serializers import ProductSerializer
 from .models import Order, OrderDetail
 
 
 class OrderDetailSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
 
     class Meta:
         model = OrderDetail
-        fields = ('cuantity', 'order', 'price')
+        fields = ('cuantity', 'product',)
 
 
 class OrderSerializer(serializers.ModelSerializer):
     total_price = serializers.SerializerMethodField()
     total_price_usd = serializers.SerializerMethodField()
-    order_details = OrderDetailSerializer(many=True, read_only=True)
+    order_details = OrderDetailSerializer(
+        many=True, source='orderdetail_set')
+
+    def get_total_price(self, instance):
+
+        return instance.get_total()
+
+    def get_total_price_usd(self, instance):
+        return instance.get_total_usd()
 
     class Meta:
         model = Order
         fields = ('id',
                   'date_time',
+                  'order_details',
+                  'total_price',
+                  'total_price_usd',
                   )
-        extra_kwargs = {
-            'id': {'read_only': True},
-            'total_price': {'read_only': True},
-            'total_price_usd': {'read_only': True}
+        read_only_fields = ('id', 'total_price', 'total_price_usd')
 
-        }
 
-    def get_total_price(self, value):
-        pass
+class OrderDetailCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderDetail
+        fields = ('cuantity', 'product',)
 
-    def get_total_price_usd(self, value):
-        pass
+    def validate(self, data):
+
+        if data['cuantity'] <= 0:
+            raise ValidationError(
+                detail=('cuantity must be greater than 0'))
+
+        remaining_stock = data['product'].stock - data['cuantity']
+        if remaining_stock < 0:
+            raise ValidationError(
+                detail=(f'out of stock, stock:{data["product"].stock}'))
+
+        self.context['remaining_stock'] = remaining_stock
+        return data
+
+    def create(self, validated_data):
+        instance = OrderDetail.objects.create(
+            order=self.context['order'], **validated_data)
+        validated_data['product'].stock = self.context['remaining_stock']
+        validated_data['product'].save()
+        return instance
+
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    orderdetail_set = OrderDetailCreateSerializer(
+        many=True, source='orderdetail_set')
+
+    class Meta:
+        model = Order
+        fields = (
+
+            'order_details',
+        )
--- ecommerce/apps/order/views.py
+++ ecommerce/apps/order/views.py
@@ -1,5 +1,34 @@
-from rest_framework import viewsets
+from multiprocessing import context
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
+from .serializers import OrderCreateSerializer, OrderDetailCreateSerializer, OrderSerializer
+
+from .models import Order
 # Create your views here.
+
+
 class OrderViewSet(viewsets.ModelViewSet):
-    pass
+    queryset = Order.objects.all()
+    serializers = {
+        'default': OrderSerializer,
+        'create': OrderCreateSerializer,
+        'update_stock': OrderSerializer,
+    }
+
+    def get_serializer_class(self):
+        """
+        returns serializer depending on the field  self.action
+        """
+        return self.serializers.get(
+            self.action, self.serializers["default"])
+
+    def create(self, request, *args, **kwargs):
+        instance = Order.objects.create()
+
+        serializer = OrderDetailCreateSerializer(
+            data=request.data['order_details'], many=True, context={'order': instance})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        serializer_response = OrderSerializer(instance)
+        return Response(serializer_response.data, status=status.HTTP_201_CREATED)
